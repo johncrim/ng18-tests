cce41b5738eb396ac9e8c19a8f399b7f4631395a
John Crim <johncrim@users.noreply.github.com>
2024-10-08 17:13:04 -0700

    refactor(tests): Use spectator

--- projects/output-emit/src/app/list/list.component.spec.ts
+++ projects/output-emit/src/app/list/list.component.spec.ts
@@ -1,23 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
 
 describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  let spectator: SpectatorHost<ListComponent<string>>;
+  const createHost = createHostFactory(ListComponent<string>);
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    spectator = createHost(`<app-list/>`);
+    expect(spectator.component).toBeTruthy();
   });
 });
